d3e2b35867c0a94b465ac04af0997a5815f7359a
rbgh1593@gmail.com <ekvmdl123@gmail.com>
2023-12-14 18:44:13 +0900

    [2020010502][김민규][14주차][확인문제]
    [2020010502][김민규][14주차][확인문제]

---
+++ [2020010502][김민규][14주차][주교재, pp.581, 문제(3)-파일입출력함수]/main.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(void)
+{
+	FILE* fp;							// 파일 포인터
+	char str[20] = "empty";				// 문자열 입력 배열
+	int ch;								// 문자 입력 변수
+
+	fp = fopen("a.txt", "r");			// 파일 개방
+	ch = fgetc(fp);						// 문자 입력
+	while (fgetc(fp) != E0F);			// 버퍼 비움
+
+	fgets(str, sizeof(str), fp);		// 한 줄 입력
+	printf("%s", str);					// 출력
+	fclose(fp);							// 파일 닫음
+
+	return 0;
+}
